059ff0b2c6d750be27946c0de080d4928fcc546b
pivanrios <pipr96@gmail.com>
2024-04-21 22:26:06 -0700

    intial commit

---
+++ orders/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Orders from './assets/components/Orders'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Orders />
+    </>
+  )
+}
+
+export default App
