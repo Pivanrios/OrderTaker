4b1eba97758aefd4a2825e29896ad06fe856af00
pivanrios <pipr96@gmail.com>
2024-04-21 23:10:48 -0700

    display orders

--- orders/src/assets/components/Orders.jsx
+++ orders/src/assets/components/Orders.jsx
@@ -1,10 +1,33 @@
-
+const ordersDb = [{
+  id: 1,
+  date: "12-03-2024",
+  idCustomer: 1322,
+  custumerName: "Pedro Perez",
+  order: [{name:"lasagna", price:8}],
+  qty:1,
+  status: ["progress"]
+},
+{
+  id: 2,
+  date: "12-03-2024",
+  idCustomer: 1222,
+  custumerName: "Marlene Gonzalez",
+  order: [{name:"lasagna", price:8}],
+  qty:1,
+  status: ["progress"]
+}]
 
 function Orders() {
   return (
     <>
         <h2>Orders</h2>
-        
+        <div>
+          {ordersDb.map(order => {
+            return(
+              <span>{order.date}{order.custumerName}{order.qty}</span>
+            )
+          })}
+        </div>
     </>
 
   )
