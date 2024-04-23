0fc221da268106ffaabd86a6fd2049e454efc9c3
pivanrios <pipr96@gmail.com>
2024-04-22 19:36:27 -0700

    add navbar and order list with orders

--- orders/src/assets/components/Orders.jsx
+++ orders/src/assets/components/Orders.jsx
@@ -15,6 +15,15 @@ const ordersDb = [{
   order: [{name:"lasagna", price:8}],
   qty:1,
   status: ["progress"]
+},
+{
+  id: 3,
+  date: "13-03-2024",
+  idCustomer: 1222,
+  custumerName: "Marlene Gonzalez",
+  order: [{name:"brownie", price:8}],
+  qty:1,
+  status: ["progress"]
 }]
 
 function Orders() {
@@ -24,7 +33,15 @@ function Orders() {
         <div>
           {ordersDb.map(order => {
             return(
-              <span>{order.date}{order.custumerName}{order.qty}</span>
+              <div className="container">
+                <ol className="container">
+                  <li>{order.id}</li>
+                  <li>{order.date}</li>
+                  <li>{order.custumerName}</li>
+                  <li>{order.order[0].name}</li>
+                  <li>{order.qty}</li>
+                </ol>
+              </div>
             )
           })}
         </div>
