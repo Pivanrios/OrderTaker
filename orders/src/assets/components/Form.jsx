63ad3ac80b8c5a6778e971f905598d54fabb9915
pivanrios <pipr96@gmail.com>
2024-04-26 23:17:26 -0700

    add condition

--- orders/src/assets/components/Form.jsx
+++ orders/src/assets/components/Form.jsx
@@ -17,7 +17,10 @@ function Form() {
   const addOrder = async (e)=>{
     e.preventDefault();//prevenrts refresh and allows to start fucntion
     console.log("start to add order");
-    await addDoc(ordersCollection, {date:newDate, customerName:newName, platillos:newDish, breakroom:newBreakroom, total:newTotal});//add new document 
+    //add validation
+    if((newDate.length > 0) && (newName.length > 0) && (newDish.length > 0) && (newBreakroom.length > 0)){ //conditions to add doc to database...
+      await addDoc(ordersCollection, {date:newDate, customerName:newName, platillos:newDish, breakroom:newBreakroom, total:newTotal});//add new document 
+    }
   };
 
   return (
@@ -25,11 +28,11 @@ function Form() {
     <h2>Take Order</h2>
     <form action="" className="container-col">
         <label htmlFor="">Order Number #</label>
-        <label htmlFor="">Date: <input type="date" onChange={(event)=>{setDate(event.target.value)}}/></label>
-        <label htmlFor="">Name: <input type="text" onChange={(event)=>{setName(event.target.value)}}/></label>
-        <label htmlFor="">Platillo: <input type="text" onChange={(event)=>{setDish(event.target.value)}}/></label>
-        <label htmlFor="">Breakroom: <input type="text" onChange={(event)=>{setBreakroom(event.target.value)}}/></label>
-        <label htmlFor="">Total:<input type="number" onChange={(event)=>{setTotal(event.target.value)}}/></label>
+        <label htmlFor="">Date: <input type="date" required onChange={(event)=>{setDate(event.target.value)}}/></label>
+        <label htmlFor="">Name: <input type="text" required onChange={(event)=>{setName(event.target.value)}}/></label>
+        <label htmlFor="">Platillo: <input type="text" required onChange={(event)=>{setDish(event.target.value)}}/></label>
+        <label htmlFor="">Breakroom: <input type="text" required onChange={(event)=>{setBreakroom(event.target.value)}}/></label>
+        <label htmlFor="">Total:<input type="number" required onChange={(event)=>{setTotal(event.target.value)}}/></label>
         <button onClick={(e)=>{addOrder(e)}}>Submit</button>
     </form>
     </>
