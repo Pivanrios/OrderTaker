e54e343164f4cd0abd3082213570b823a6c40a62
pivanrios <pipr96@gmail.com>
2024-04-26 23:38:02 -0700

    migrate to routes

--- orders/src/App.jsx
+++ orders/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import Orders from './assets/components/Orders'
 import './App.css'
-import Navbar from './assets/components/Navbar'
-import Form from './assets/components/Form'
 
 function App() {
 
   return (
-    <>
-      <Navbar />
-      <Orders />
-      <Form />
+    <>    
     </>
   )
 }
-
 export default App
--- orders/src/main.jsx
+++ orders/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import Orders from './assets/components/Orders.jsx';
+import Form from './assets/components/Form.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+     <Routes>
+        <Route path='orders' element={<Orders/>}/>
+        <Route path='/' element={<Form/>}/>
+      </Routes>
+  </BrowserRouter>,
 )
