799d88ee9393eb4558ef61b95e8bd3f245812d01
pivanrios <pipr96@gmail.com>
2024-04-27 00:10:13 -0700

    update to data api

--- orders/src/main.jsx
+++ orders/src/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import { Route,RouterProvider,Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Orders from './assets/components/Orders.jsx';
 import Form from './assets/components/Form.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-     <Routes>
+const router = createBrowserRouter(//version 6.4 of react-router
+  createRoutesFromElements(
+      <Route path='/' >
+        <Route path='takeOrder' element={<Form/>}/>
         <Route path='orders' element={<Orders/>}/>
-        <Route path='/' element={<Form/>}/>
-      </Routes>
-  </BrowserRouter>,
+      </Route>
+  )
+);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
